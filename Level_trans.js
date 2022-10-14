421408d412a606cb0b4a9fec20adf1b160fc33f8
Natchanon19 <s63010722510061@email.kmutnb.ac.th>
2022-10-14 16:16:30 +0700

    Fix1

--- Level_trans.js
+++ Level_trans.js
@@ -6,17 +6,19 @@ document.addEventListener('DOMContentLoaded',function (){
     .then(data => {
         const Sensor = document.querySelector('#Sensor').value;
         document.querySelector('#Sensor').value ='';
-        const Name = 'name:' + " " + data[Sensor].name;
-        const Type = 'type:' + " " + data[Sensor].type;
-        const Unit = 'unit:' + " " + data[Sensor].unit;
-        const Range = 'range:' + " " + data[Sensor].range;
-        const Plantsite = 'plant-site:' + " " + data[Sensor]["plant-site"];
+        const Name = 'Name:' + " " + data[Sensor].name;
+        const Type = 'Type:' + " " + data[Sensor].type;
+        const Unit = 'Unit:' + " " + data[Sensor].unit;
+        const Range = 'Range:' + " " + data[Sensor].range;
+        const Plantsite = 'Plant-site:' + " " + data[Sensor]["plant-site"];
+        const Value = 'Value:' + " " + data[Sensor].value;
         if(Sensor <= "3"){
             document.querySelector('#result0').innerHTML = `${Name}.`;
             document.querySelector('#result1').innerHTML = `${Type}.`;
             document.querySelector('#result2').innerHTML = `${Unit}.`;
             document.querySelector('#result3').innerHTML = `${Range}.`;
             document.querySelector('#result4').innerHTML = `${Plantsite}.`;
+            document.querySelector('#result5').innerHTML = `${Value}.`;
         }
         else{
             document.querySelector('#result').innerHTML = "Error";
--- Press_trans.js
+++ Press_trans.js
@@ -1,31 +1,4 @@
 document.addEventListener('DOMContentLoaded',function (){
-    ///////////////////////////////////////////////////////////////////// Post method ///////////////////////////////////////////////////////////////////////
-//     let Postpls = document.querySelector('#Postpls');
-//     Postpls.addEventListener('submit',createData);
-//     function createData(){
-//         alert("Hello! I am an alert box!!")
-//     fetch('https://midterm-exam-010723313-2022.herokuapp.com/student_post',{
-//         method: 'post',
-//         headers: {
-//             'Content-Type' : 'application/json'
-//         },
-//         body: JSON.stringify({
-//             "st_id" :document.querySelector('#new_id').value,
-//             "st_name" : document.querySelector('#new_Sname').value,
-//             "sensor_name" : document.querySelector('#new_name').value,
-//             "sensor_type" : document.querySelector('#new_type').value,
-//             "sensor_unit" : document.querySelector('#new_unit').value,
-//             "sensor_value": document.querySelector('#new_value').value
-//         })
-//     })
-//     .then(response => console.log(response))
-//     .catch(error => {
-//         console.log('Error:', error);
-//     });
-//     return false;
-// }
-///////////////////////////////////////////////////////////////////// Post method ///////////////////////////////////////////////////////////////////////
-
 ///////////////////////////////////////////////////////////////////// Get method ///////////////////////////////////////////////////////////////////////
     // document.querySelector('select').onchange = function() {
     // const get_link = document.querySelector('#Links').value;
@@ -35,17 +8,19 @@ document.addEventListener('DOMContentLoaded',function (){
     .then(data => {
         const Sensor = document.querySelector('#Sensor').value;
         document.querySelector('#Sensor').value ='';
-        const Name = 'name:' + " " + data[Sensor].name;
-        const Type = 'type:' + " " + data[Sensor].type;
-        const Unit = 'unit:' + " " + data[Sensor].unit;
-        const Range = 'range:' + " " + data[Sensor].range;
-        const Plantsite = 'plant-site:' + " " + data[Sensor]["plant-site"];
+        const Name = 'Name:' + " " + data[Sensor].name;
+        const Type = 'Type:' + " " + data[Sensor].type;
+        const Unit = 'Unit:' + " " + data[Sensor].unit;
+        const Range = 'Range:' + " " + data[Sensor].range;
+        const Plantsite = 'Plant-site:' + " " + data[Sensor]["plant-site"];
+        const Value = 'Value:' + " " + data[Sensor].value;
         if(Sensor <= "3"){
             document.querySelector('#result0').innerHTML = `${Name}.`;
             document.querySelector('#result1').innerHTML = `${Type}.`;
             document.querySelector('#result2').innerHTML = `${Unit}.`;
             document.querySelector('#result3').innerHTML = `${Range}.`;
             document.querySelector('#result4').innerHTML = `${Plantsite}.`;
+            document.querySelector('#result5').innerHTML = `${Value}.`;
         }
         else{
             document.querySelector('#result').innerHTML = "Error";
